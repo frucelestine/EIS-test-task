186862368c4018032f71f715888bc14c73442131
celes94 <celesclaks@yahoo.com>
2020-06-02 11:12:13 +0200

    Updates

---
+++ precache-manifest.0201b146f337c31d7948fe6e880809ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80b5a00ece00e7fc4ee54c677291e3ee",
+    "url": "/EIS-test-task/index.html"
+  },
+  {
+    "revision": "a9b93fa6a8adb2ef6a9f",
+    "url": "/EIS-test-task/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ac74dc2e742e0a5d482d",
+    "url": "/EIS-test-task/static/js/2.fb291e93.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/EIS-test-task/static/js/2.fb291e93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9b93fa6a8adb2ef6a9f",
+    "url": "/EIS-test-task/static/js/main.2ace4f36.chunk.js"
+  },
+  {
+    "revision": "072101edd2db3968efb8",
+    "url": "/EIS-test-task/static/js/runtime-main.73001334.js"
+  }
+]);
